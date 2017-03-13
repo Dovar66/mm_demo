660018416eadf9009127e99161fc3f1ad01ceadf
Dovar66 <xiaohe0949@163.com>
2017-03-13 22:46:49 +0800

    代码重新整理

--- app/src/main/java/com/tencent/test/AutoService.java
+++ app/src/main/java/com/tencent/test/AutoService.java
@@ -1,6 +1,7 @@
 package com.tencent.test;
 
 import android.accessibilityservice.AccessibilityService;
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.ClipData;
@@ -20,10 +21,10 @@ import android.widget.Toast;
 import java.util.List;
 
 /**
- * 1、确保应用已获取到悬浮窗权限Manifest.permission.SYSTEM_ALERT_WINDOW，部分手机可能需要手动进入权限管理中开启
+ * 1、确保应用已获取到悬浮窗权限Manifest.permission.SYSTEM_ALERT_WINDOW，用于功能的即时开启与关闭，部分手机可能需要手动进入权限管理中开启
  * 2、使用此服务需要获取手机特殊权限：部分手机点击本demo页面中“打开辅助服务”按钮进入辅助功能页即可找到名称为“mm”的服务，然后打开即可，
  * 其他手机需要在辅助功能中找到“无障碍”项，然后在“无障碍”中找到“mm”打开即可
- * Created by Dovar66 on 2016/9/21 0021.
+ * Created by Dovar66
  */
 public class AutoService extends AccessibilityService implements View.OnClickListener {
     private static final String TAG = "test";
@@ -44,11 +45,11 @@ public class AutoService extends AccessibilityService implements View.OnClickLis
      */
     private String hello = "测试APP自动打招呼功能，这是一条测试信息";
 
-    private boolean startFunc2 = false;//标记是否开启自动添加附近的人为好友的功能;
+    private boolean enableFunc2;//标记是否开启抢红包功能
+    private boolean enableFunc3;//标记是否开启自动添加附近的人为好友的功能;
     private int i = 0;//记录已打招呼的人数
-    private int page=1;//记录附近的人列表页码,初始页码为1
+    private int page = 1;//记录附近的人列表页码,初始页码为1
     private int prepos = -1;//记录页面跳转来源，0--从附近的人页面跳转到详细资料页，1--从打招呼页面跳转到详细资料页
-
 
 
     @Override
@@ -61,15 +62,34 @@ public class AutoService extends AccessibilityService implements View.OnClickLis
             if (!texts.isEmpty()) {
                 for (CharSequence t : texts) {
                     String text = String.valueOf(t);
+                    if (text.contains("微信红包")){
+                        openNotification(event);
+                    }
+
+                    //此代码段用于实现自动打开公众号或个人推送过来的链接
                     if (text.contains(PUSH_TEXT_KEY)) {
                         openNotification(event);
                         openDelay(1000, URL_TEXT_KEY);
                     }
+
                 }
             }
         }
+        //抢红包
+        if (enableFunc2) {
+            if ("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI".equals(event.getClassName())) {
+                //当前在红包待开页面，去拆红包
+                getLuckyMoney();
+            } else if ("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI".equals(event.getClassName())) {
+                //拆完红包后看详细纪录的界面
+                openNext("查看我的红包记录");
+            } else if ("com.tencent.mm.ui.LauncherUI".equals(event.getClassName())) {
+                //在聊天界面,去点中红包
+                openLuckyEnvelope();
+            }
+        }
         //自动加人
-        if (!startFunc2) {
+        if (!enableFunc3) {
             return;
         }
         if (eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED && event.getClassName().equals("com.tencent.mm.ui.LauncherUI")) {
@@ -85,10 +105,10 @@ public class AutoService extends AccessibilityService implements View.OnClickLis
             AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
             List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText("米以内");
             Log.d("name", "附近的人列表人数: " + list.size());
-            if (i < (list.size()*page) ){
-                list.get(i%list.size()).performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                list.get(i%list.size()).getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
-            } else if (i == list.size()*page) {
+            if (i < (list.size() * page)) {
+                list.get(i % list.size()).performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                list.get(i % list.size()).getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            } else if (i == list.size() * page) {
                 //本页已全部打招呼，所以下滑列表加载下一页，每次下滑的距离是一屏
                 for (int i = 0; i < nodeInfo.getChild(0).getChildCount(); i++) {
                     if (nodeInfo.getChild(0).getChild(i).getClassName().equals("android.widget.ListView")) {
@@ -105,7 +125,7 @@ public class AutoService extends AccessibilityService implements View.OnClickLis
                                 }
                                 AccessibilityNodeInfo nodeInfo_ = getRootInActiveWindow();
                                 List<AccessibilityNodeInfo> list_ = nodeInfo_.findAccessibilityNodeInfosByText("米以内");
-                                Log.d("name", "列表人数: "+list_.size());
+                                Log.d("name", "列表人数: " + list_.size());
                                 //滑动之后，上一页的最后一个item为当前的第一个item，所以从第二个开始打招呼
                                 list_.get(1).performAction(AccessibilityNodeInfo.ACTION_CLICK);
                                 list_.get(1).getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
@@ -161,7 +181,7 @@ public class AutoService extends AccessibilityService implements View.OnClickLis
         if (event.getParcelableData() == null || !(event.getParcelableData() instanceof Notification)) {
             return;
         }
-        //以下是精华，将微信的通知栏消息打开
+        //将通知栏消息打开
         Notification notification = (Notification) event.getParcelableData();
         PendingIntent pendingIntent = notification.contentIntent;
         try {
@@ -173,12 +193,14 @@ public class AutoService extends AccessibilityService implements View.OnClickLis
 
     /**
      * 点击匹配的nodeInfo
+     *
      * @param str text关键字
      */
     private void openNext(String str) {
         AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
         if (nodeInfo == null) {
             Log.w(TAG, "rootWindow为空");
+            Toast.makeText(this, "rootWindow为空", Toast.LENGTH_SHORT).show();
             return;
         }
         List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText(str);
@@ -186,16 +208,9 @@ public class AutoService extends AccessibilityService implements View.OnClickLis
         if (list.size() > 0) {
             list.get(list.size() - 1).performAction(AccessibilityNodeInfo.ACTION_CLICK);
             list.get(list.size() - 1).getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        } else {
+            Toast.makeText(this, "找不到有效的节点", Toast.LENGTH_SHORT).show();
         }
-//        if ("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI".equals(event.getClassName())) {
-//            //点中了红包，下一步就是去拆红包
-//            checkKey1();
-//        } else if ("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI".equals(event.getClassName())) {
-//            //拆完红包后看详细的纪录界面
-//        } else if ("com.tencent.mm.ui.LauncherUI".equals(event.getClassName())) {
-//            //在聊天界面,去点中红包
-//            checkKey2();
-//        }
     }
 
     //延迟打开界面
@@ -238,7 +253,7 @@ public class AutoService extends AccessibilityService implements View.OnClickLis
     @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
-        Toast.makeText(this, "连接服务", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "服务已开启", Toast.LENGTH_SHORT).show();
     }
 
 //    private void sendNotificationEvent() {
@@ -255,49 +270,51 @@ public class AutoService extends AccessibilityService implements View.OnClickLis
 //    }
 
 
-//    private void checkKey1() {
-//        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
-//        if (nodeInfo == null) {
-//            Log.w(TAG, "rootWindow为空");
-//            return;
-//        }
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    private void getLuckyMoney() {
+        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+        if (nodeInfo == null) {
+            Log.w(TAG, "rootWindow为空");
+            Toast.makeText(this, "rootWindow为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //bi3是本人写代码时微信拆红包的button的id,该id可能会在更新微信版本后发生变更,可通过Android Device Monitor查看获取
+        //可创建一个hashMap,在微信发生版本变更时储存对应微信版本号与id值，用于适配多个微信版本
+        List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByViewId("bi3");
 //        List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText("拆红包");
-//        int c = nodeInfo.getChildCount();
-//        int i;
-//        for (i = 0; i < c; i++) {
-//            nodeInfo.getChild(i).performAction(AccessibilityNodeInfo.ACTION_CLICK);
-//        }
-//        for (AccessibilityNodeInfo n : list) {
-//            n.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-//        }
-//    }
+        if (list != null && list.size() > 0) {
+            list.get(0).performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            return;
+        }
+        int c = nodeInfo.getChildCount();
+        int i;
+        for (i = 0; i < c; i++) {
+            nodeInfo.getChild(i).performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        }
+    }
 
-//    private void checkKey2() {
-//        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
-//        if (nodeInfo == null) {
-//            Log.w(TAG, "rootWindow为空");
-//            return;
-//        }
-//        List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText("领取红包");
-//        if (list.isEmpty()) {
-//            list = nodeInfo.findAccessibilityNodeInfosByText(PUSH_TEXT_KEY);
-//            for (AccessibilityNodeInfo n : list) {
-//                Log.i(TAG, "-->微信红包:" + n);
-//                n.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-//                break;
-//            }
-//        } else {
-//            //最新的红包领起
-//            for (int i = list.size() - 1; i >= 0; i--) {
-//                AccessibilityNodeInfo parent = list.get(i).getParent();
-//                Log.i(TAG, "-->领取红包:" + parent);
-//                if (parent != null) {
-//                    parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-//                    break;
-//                }
-//            }
-//        }
-//    }
+    private void openLuckyEnvelope() {
+        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+        if (nodeInfo == null) {
+            Log.w(TAG, "rootWindow为空");
+            Toast.makeText(this, "rootWindow为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText("领取红包");
+        if (list.isEmpty()) {
+            Toast.makeText(this, "找不到红包", Toast.LENGTH_SHORT).show();
+        } else {
+            //选择聊天记录中最新的红包
+            for (int i = list.size() - 1; i >= 0; i--) {
+                AccessibilityNodeInfo parent = list.get(i).getParent();
+                Log.i(TAG, "-->领取红包:" + parent);
+                if (parent != null) {
+                    parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                    break;
+                }
+            }
+        }
+    }
 
     @Override
     public void onCreate() {
@@ -305,7 +322,8 @@ public class AutoService extends AccessibilityService implements View.OnClickLis
         createFloatView();
     }
 
-    private Button floatbtn;
+    private Button floatBtn1;
+    private Button floatBtn2;
 
     //创建悬浮按钮
     private void createFloatView() {
@@ -314,28 +332,42 @@ public class AutoService extends AccessibilityService implements View.OnClickLis
         pl.type = WindowManager.LayoutParams.TYPE_PHONE;
         pl.format = PixelFormat.RGBA_8888;
         pl.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
-        pl.gravity = Gravity.RIGHT | Gravity.BOTTOM;
+        pl.gravity = Gravity.END | Gravity.BOTTOM;
         pl.x = 0;
         pl.y = 0;
 
-        pl.width = 200;
-        pl.height = 200;
+        pl.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        pl.height = WindowManager.LayoutParams.WRAP_CONTENT;
 
         LayoutInflater inflater = LayoutInflater.from(this);
-        floatbtn = (Button) inflater.inflate(R.layout.floatbtn, null);
-        wm.addView(floatbtn, pl);
+        floatBtn1 = (Button) inflater.inflate(R.layout.floatbtn, null);
+        floatBtn1.setText("打招呼");
+        floatBtn2 = (Button) inflater.inflate(R.layout.floatbtn, null);
+        floatBtn2.setText("抢红包");
+        wm.addView(floatBtn1, pl);
+        pl.gravity = Gravity.BOTTOM|Gravity.START;
+        wm.addView(floatBtn2, pl);
 
-        floatbtn.setOnClickListener(this);
-
+        floatBtn1.setOnClickListener(this);
+        floatBtn2.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (startFunc2) {
-            floatbtn.setText("启用加人");
+        if (v == floatBtn1) {
+            if (enableFunc3) {
+                floatBtn1.setText("打招呼");
+            } else {
+                floatBtn1.setText("off");
+            }
+            enableFunc3 = !enableFunc3;
         } else {
-            floatbtn.setText("停止加人");
+            if (enableFunc2) {
+                floatBtn2.setText("抢红包");
+            } else {
+                floatBtn2.setText("off");
+            }
+            enableFunc2 = !enableFunc2;
         }
-        startFunc2 = !startFunc2;
     }
 }
