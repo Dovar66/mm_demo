245aa2b3e7f6bbc14a9b7a79b21aaaaee32eded0
heweizong@itouchtv.cn <heweizong@itouchtv.cn>
2017-10-30 18:38:40 +0800

    fixing

--- app/src/main/java/com/tencent/test/AutoService.java
+++ app/src/main/java/com/tencent/test/AutoService.java
@@ -1,7 +1,6 @@
 package com.tencent.test;
 
 import android.accessibilityservice.AccessibilityService;
-import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.ClipData;
@@ -59,7 +58,61 @@ public class AutoService extends AccessibilityService {
     @Override
     public void onAccessibilityEvent(final AccessibilityEvent event) {
         int eventType = event.getEventType();
-        String action = "action";
+        String str_eventType;
+        switch (eventType) {
+            case AccessibilityEvent.TYPE_VIEW_CLICKED:
+                Log.i(TAG, "==============Start====================");
+                str_eventType = "TYPE_VIEW_CLICKED";
+                AccessibilityNodeInfo noteInfo = event.getSource();
+                Log.i(TAG, noteInfo.toString());
+                Log.i(TAG, "=============END=====================");
+                break;
+            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
+                str_eventType = "TYPE_VIEW_FOCUSED";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED:
+                str_eventType = "TYPE_VIEW_LONG_CLICKED";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_SELECTED:
+                str_eventType = "TYPE_VIEW_SELECTED";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:
+                str_eventType = "TYPE_VIEW_TEXT_CHANGED";
+                break;
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+                str_eventType = "TYPE_WINDOW_STATE_CHANGED";
+                break;
+            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
+                str_eventType = "TYPE_NOTIFICATION_STATE_CHANGED";
+                break;
+            case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_END:
+                str_eventType = "TYPE_TOUCH_EXPLORATION_GESTURE_END";
+                break;
+            case AccessibilityEvent.TYPE_ANNOUNCEMENT:
+                str_eventType = "TYPE_ANNOUNCEMENT";
+                break;
+            case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_START:
+                str_eventType = "TYPE_TOUCH_EXPLORATION_GESTURE_START";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_HOVER_ENTER:
+                str_eventType = "TYPE_VIEW_HOVER_ENTER";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_HOVER_EXIT:
+                str_eventType = "TYPE_VIEW_HOVER_EXIT";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_SCROLLED:
+                str_eventType = "TYPE_VIEW_SCROLLED";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED:
+                str_eventType = "TYPE_VIEW_TEXT_SELECTION_CHANGED";
+                break;
+            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
+                str_eventType = "TYPE_WINDOW_CONTENT_CHANGED";
+                break;
+            default:
+                str_eventType = String.valueOf(eventType);
+        }
+        String action;
         switch (event.getAction()) {
             case AccessibilityNodeInfo.ACTION_ACCESSIBILITY_FOCUS:
                 action = "ACTION_ACCESSIBILITY_FOCUS";
@@ -82,11 +135,20 @@ public class AutoService extends AccessibilityService {
             case AccessibilityNodeInfo.ACTION_SCROLL_BACKWARD:
                 action = "ACTION_SCROLL_BACKWARD";
                 break;
+            case AccessibilityNodeInfo.ACTION_FOCUS:
+                action = "ACTION_FOCUS";
+                break;
+            case 0://com.android.systemui 一般是系统标题栏内容发生改变
+                //在微信内页但不在聊天详情页时，收到红包不会产生通知栏事件，只有系统标题栏内容发生改变
+                //标题栏中已经有微信未读消息图标时：TYPE_WINDOW_CONTENT_CHANGED	package:com.android.systemui	Class:android.widget.ImageView
+                //标题栏中尚未有未读消息图标时：TYPE_WINDOW_CONTENT_CHANGED	package:com.android.systemui	Class:android.widget.FrameLayout
+                Log.i(TAG, "EventType: " + str_eventType + "\tpackage:" + event.getPackageName() + "\tClass:" + event.getClassName() + "\t");
+                return;
             default:
                 action = event.getAction() + "";
                 break;
         }
-        Log.v(TAG, "EventType: " + eventType + "\tAction:" + action + "\tPackage:" + event.getPackageName() + "\tClass:" + event.getClassName() + "\t");
+        Log.v(TAG, "EventType: " + str_eventType + "\tAction:" + action + "\tpackage:" + event.getPackageName() + "\tClass:" + event.getClassName() + "\t");
 
         if (eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED && MainActivity.canShowWindow(this)) {
             currentActivity = String.valueOf(event.getClassName());
@@ -103,8 +165,7 @@ public class AutoService extends AccessibilityService {
                         for (CharSequence t : texts) {
                             String text = String.valueOf(t);
 
-                            if (enableFunc2
-                                    && text.contains("微信红包")) {
+                            if (text.contains("微信红包")) {
                                 isRunning = true;
                                 isBackward = false;
                                 openNotification(event);
@@ -264,7 +325,6 @@ public class AutoService extends AccessibilityService {
     private void openNext(String str) {
         AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
         if (nodeInfo == null) {
-            Log.d(TAG, "rootWindow为空");
             Toast.makeText(this, "rootWindow为空", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -379,7 +439,6 @@ public class AutoService extends AccessibilityService {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
             AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();  //获得整个窗口对象
             if (nodeInfo == null) {
-                Log.d(TAG, "rootWindow为空");
                 Toast.makeText(this, "rootWindow为空", Toast.LENGTH_SHORT).show();
                 return;
             }
@@ -412,7 +471,6 @@ public class AutoService extends AccessibilityService {
     private void openLuckyEnvelope() {
         AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
         if (nodeInfo == null) {
-            Log.d(TAG, "rootWindow为空");
             Toast.makeText(this, "rootWindow为空", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -448,7 +506,7 @@ public class AutoService extends AccessibilityService {
                 }
             }
         } else {
-            Toast.makeText(this, "没有新红包", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "openChatPage: 没有新红包");
         }
     }
 }
